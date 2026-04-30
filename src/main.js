bef441b9962ff2cc6df7209ab4b08b92e28edfa0
unknown <huangbingzhang@cn.net.ntes>
2026-04-30 20:13:04 +0800

    feat(P1): wire up Pinia + PrimeVue unstyled

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import ConfirmationService from 'primevue/confirmationservice'
 import App from './App.vue'
 
 const app = createApp(App)
+app.use(createPinia())
+app.use(PrimeVue, { unstyled: true })
+app.use(ToastService)
+app.use(ConfirmationService)
 app.mount('#app')
